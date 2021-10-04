b8e3a544a55224bf7df55b2b11215ff84c501ce1
Elou <Elou@192.168.0.16>
2021-10-04 10:14:39 +0200

    Premier exo TP1

---
+++ manipNombresInt_BAROIS_BERNARD/src/manipnombresint_barois_bernard/ManipNombresInt_BAROIS_BERNARD.java
@@ -0,0 +1,34 @@
+/*
+ * TP1 Exercice 1
+ * Elouan Barois Loïc Bernard
+ * Septembre 2021
+ */
+package manipnombresint_barois_bernard;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Elou
+ */
+public class ManipNombresInt_BAROIS_BERNARD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int var1 =0;
+    int var2 =0;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Entrez 2 entiers: ");
+    var1 = sc.nextInt();
+    var2 = sc.nextInt();
+    System.out.println(var1+" "+var2);
+    System.out.println("somme ="+(var1+var2));
+    System.out.println("diff ="+(var1-var2));
+    System.out.println("produit ="+var1*var2);
+    System.out.println("quotient entier ="+var1/var2+" et reste de la division euclidienne ="+var1%var2);
+
+    
+    }
+}
