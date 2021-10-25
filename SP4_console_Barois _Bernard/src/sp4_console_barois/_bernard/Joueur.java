109fae8ddc53b2af82951cf2073c8918b5d4ed47
Elou <Elou@192.168.0.16>
2021-10-25 12:57:58 +0200

    Début Puissance 4

---
+++ SP4_console_Barois _Bernard/src/sp4_console_barois/_bernard/Joueur.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_barois._bernard;
+
+/**
+ *
+ * @author Elou
+ */
+public class Joueur {
+    String nom;
+    String Couleur;
+    Jeton ListeJeton []= new Jeton [21]; 
+    int nombreJetonsRestants;
+    public Joueur(String Unnom){
+        nom=Unnom;
+    }
+    
+    public void affecterCouleur(String UneCouleur){
+        Couleur=UneCouleur;
+    }
+    
+    public void ajouterJeton(Jeton UnJeton){
+        ListeJeton[nombreJetonsRestants]=UnJeton;
+    }
+    
+}
