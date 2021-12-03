9786402654136cb85d885e3d9198a8584ed62038
loic1 <loic1@192.168.115.99>
2021-12-03 18:23:17 +0100

    Association button avec image clair/foncée

--- LIGHTS_OUT_Console_Barois_Bernard_Mode_Graphique/src/lights/out/CelluleGraphique.java
+++ LIGHTS_OUT_Console_Barois_Bernard_Mode_Graphique/src/lights/out/CelluleGraphique.java
@@ -1,9 +1,35 @@
 
 package lights.out;
 
+import java.awt.Graphics;
+import java.util.Random;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 
 public class CelluleGraphique extends JButton{
     Cellule celluleAssociee ;
+    ImageIcon caseClair = new javax.swing.ImageIcon(getClass().getResource("/Images/caseClair.drawio.png")) ;
+    ImageIcon caseFoncee = new javax.swing.ImageIcon(getClass().getResource("/images/caseFoncee.drawio.png")) ;
+
+public CelluleGraphique (Cellule uneCellule){
+    celluleAssociee = uneCellule ;
+
+}
+
+public void paintComponent (Graphics G){
+        super.paintComponent(G) ;
+        Random rand = new Random();
+        int Coul=rand.nextInt(2);
+        if (Coul==1){
+            setIcon(caseClair) ;
+        }
+        else {
+            setIcon(caseFoncee) ;
+        }
+        
+        
+}
+
+
 }
