746e8f99f679d397b010895201365c4b89906d4f
Elou <Elou@192.168.0.16>
2021-12-06 16:49:02 +0100

    Création des classes grille, ligne et cellule

---
+++ MASTERMIND_Console_Barois_Bernard/src/mastermind_console_barois_bernard/Ligne.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mastermind_console_barois_bernard;
+
+/**
+ *
+ * @author Elou
+ */
+public class Ligne {
+    //Attributs
+    Cellule[] CellulesLigne= new Cellule [4];
+    int [] Indices=new int[4];
+    //Constructeur
+    
+}
