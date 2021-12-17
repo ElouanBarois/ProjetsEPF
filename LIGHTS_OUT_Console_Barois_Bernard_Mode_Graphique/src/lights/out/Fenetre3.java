ed62ff936671a8a6028fdfbd0c223d339fea4aa0
loic1 <loic1@192.168.115.99>
2021-12-17 11:11:44 +0100

    Modes facile et difficile fonctionnels

--- LIGHTS_OUT_Console_Barois_Bernard_Mode_Graphique/src/lights/out/Fenetre3.java
+++ LIGHTS_OUT_Console_Barois_Bernard_Mode_Graphique/src/lights/out/Fenetre3.java
@@ -15,7 +15,7 @@ public class Fenetre3 extends javax.swing.JFrame {
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                CelluleGraphique CellGraph = new CelluleGraphique(grilleJeu2.CellulesJeu2[i][j],i,j);
+                CelluleGraphique2 CellGraph = new CelluleGraphique2(grilleJeu2.CellulesJeu2[i][j],i,j);
                 grille_99.add(CellGraph);
             
                 for (int z=0;z<40;z++){
@@ -69,7 +69,7 @@ public class Fenetre3 extends javax.swing.JFrame {
 
         grille_99.setBackground(new java.awt.Color(255, 204, 204));
         grille_99.setLayout(new java.awt.GridLayout(9, 9));
-        getContentPane().add(grille_99, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 10, 720, 720));
+        getContentPane().add(grille_99, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 10, 729, 729));
 
         setBounds(0, 0, 1029, 912);
     }// </editor-fold>//GEN-END:initComponents
