1f84e37097d2e882efc5d2c8425d2592906fe59c
loic1 <loic1@192.168.115.99>
2021-12-22 17:14:26 +0100

    Indices

--- LIGHTS_OUT_Console_Barois_Bernard_Mode_Graphique/src/lights/out/Fenetre2.java
+++ LIGHTS_OUT_Console_Barois_Bernard_Mode_Graphique/src/lights/out/Fenetre2.java
@@ -15,8 +15,8 @@ public class Fenetre2 extends javax.swing.JFrame {
         initComponents();
         grilleJeu = new Grille ();
         but_rejouer.setVisible(false);
-        Gagne.setVisible(true);
-        
+        Gagne.setVisible(false);
+        txt_indice.setVisible(false) ;
         
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
@@ -93,6 +93,9 @@ public class Fenetre2 extends javax.swing.JFrame {
         reglesDuJeu = new javax.swing.JTextArea();
         jScrollPane2 = new javax.swing.JScrollPane();
         Gagne = new javax.swing.JTextArea();
+        indice = new javax.swing.JButton();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        txt_indice = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -127,9 +130,27 @@ public class Fenetre2 extends javax.swing.JFrame {
         Gagne.setText("\n            \n                      Bravo c'est gagné !!!");
         jScrollPane2.setViewportView(Gagne);
 
-        getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 270, 410, 140));
+        getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 270, 410, -1));
 
-        setBounds(0, 0, 1035, 605);
+        indice.setFont(new java.awt.Font("sansserif", 3, 18)); // NOI18N
+        indice.setForeground(new java.awt.Color(51, 51, 51));
+        indice.setText("Indice");
+        indice.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                indiceActionPerformed(evt);
+            }
+        });
+        getContentPane().add(indice, new org.netbeans.lib.awtextra.AbsoluteConstraints(1020, 30, 390, 80));
+
+        txt_indice.setColumns(20);
+        txt_indice.setFont(new java.awt.Font("sansserif", 3, 14)); // NOI18N
+        txt_indice.setRows(5);
+        txt_indice.setText("Vous auriez pu faire un effort...\n\nCliquez sous chaque lumière allumée en partant d'en\nhaut et en descendant petit à petit.\nPuis recommencez en cliquant une case aléatoire sur\nla ligne du haut.");
+        jScrollPane3.setViewportView(txt_indice);
+
+        getContentPane().add(jScrollPane3, new org.netbeans.lib.awtextra.AbsoluteConstraints(1020, 120, 390, 160));
+
+        setBounds(0, 0, 1452, 653);
     }// </editor-fold>//GEN-END:initComponents
 
     private void but_rejouerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_but_rejouerActionPerformed
@@ -137,6 +158,10 @@ public class Fenetre2 extends javax.swing.JFrame {
         f1.show();
         this.dispose() ;
     }//GEN-LAST:event_but_rejouerActionPerformed
+
+    private void indiceActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_indiceActionPerformed
+        txt_indice.setVisible(true) ;
+    }//GEN-LAST:event_indiceActionPerformed
 
     
     public static void main(String args[]) {
@@ -175,8 +200,11 @@ public class Fenetre2 extends javax.swing.JFrame {
     private javax.swing.JTextArea Gagne;
     private javax.swing.JButton but_rejouer;
     private javax.swing.JPanel grille_55;
+    private javax.swing.JButton indice;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JTextArea reglesDuJeu;
+    private javax.swing.JTextArea txt_indice;
     // End of variables declaration//GEN-END:variables
 }
