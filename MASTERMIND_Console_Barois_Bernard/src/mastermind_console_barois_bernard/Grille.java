fb6bc4cecffb8125bde669cc1aa56ba3925a15c8
Elou <Elou@192.168.0.16>
2022-01-02 21:20:51 +0100

    Début des commentaires pour cellule, et grille

--- MASTERMIND_Console_Barois_Bernard/src/mastermind_console_barois_bernard/Grille.java
+++ MASTERMIND_Console_Barois_Bernard/src/mastermind_console_barois_bernard/Grille.java
@@ -9,6 +9,7 @@ package mastermind_console_barois_bernard;
  * @author Elou
  */
 public class Grille {
+    //Attributs:
     Ligne [] LignesJeu = new Ligne [14] ;
     
     
@@ -19,25 +20,25 @@ public class Grille {
             }
     }
     
-    public String[] AfficherGrilleSurConsole(String[] Code,int ligneActuelle ){
+    public String[] AfficherGrilleSurConsole(String[] Code,int ligneActuelle ){ //On prend en argument le code à trouver et la ligne sur laquelle le joueur joue
         String [] B = new String [4];
-        for (int j=0;j<14;j++){
-            String [] A =LignesJeu[j].AfficherLigneSurConsole(Code);
-            if (j==ligneActuelle){
+        for (int j=0;j<14;j++){ //Boucle pour les 14 lignes
+            String [] A =LignesJeu[j].AfficherLigneSurConsole(Code); //On appelle l'affichage de chaque ligne
+            if (j==ligneActuelle){ //On sauvegarde les couleurs tentées pour la lignes actuelle
                 B = A;
             }
         }
-        return B;
+        return B; //on retourne le tableau de couleurs de la ligne actuelle
     }
     
-    public boolean EtreGagnantePourJoueur(String[] Code,String[] CouleursLigne){
+    public boolean EtreGagnantePourJoueur(String[] Code,String[] CouleursLigne){  //On prend 2 arguments qu'on va comparer
         int compt=0;
-        for (int j=0;j<4;j++){
+        for (int j=0;j<4;j++){ //On teste si chaque couleur correspond indice par indice
             if (CouleursLigne[j]==Code[j]){
                 compt++;
             }
         }
-        if (compt==4){
+        if (compt==4){ // si les 4 couleurs correspondent la grille est gagnante
             return true;
         }
         else{
